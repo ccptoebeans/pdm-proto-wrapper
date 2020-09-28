e747ab6d81ec176ca2b804cdbd4ef666089ef168
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2020-09-28 16:44:22 +0000

    Use DllExport macro for proto data

--- include/pdm/protobuf.h
+++ include/pdm/protobuf.h
@@ -2,12 +2,6 @@
 
 #include "pdm.pb.h"
 
-#if _WIN32
-#define DllExport __declspec( dllexport )
-#elif __APPLE__
-#define DllExport __attribute__((visibility("default")))
-#endif
-
 namespace pdm_proto
 {
     DllExport platform::Information GetData();
