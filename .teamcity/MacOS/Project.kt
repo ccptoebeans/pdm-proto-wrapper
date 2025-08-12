ff427914a376fdce440280b1529cf791e01b94e9
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2025-08-12 15:04:48 +0000

    Add teamcity kotlin configuration

---
+++ .teamcity/MacOS/Project.kt
@@ -0,0 +1,162 @@
+package MacOS
+
+import jetbrains.buildServer.configs.kotlin.Project
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.buildFeatures.XmlReport
+import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.buildFeatures.freeDiskSpace
+import jetbrains.buildServer.configs.kotlin.buildFeatures.nuGetFeedCredentials
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.buildFeatures.sshAgent
+import jetbrains.buildServer.configs.kotlin.buildFeatures.xmlReport
+import jetbrains.buildServer.configs.kotlin.buildSteps.exec
+import jetbrains.buildServer.configs.kotlin.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.buildSteps.python
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.buildFeatures.provideAwsCredentials
+
+val Release = CarbonBuildMacOS("Release MacOS", "Release", "nmc-universal-osx-static-release")
+
+object Project : Project({
+    id("MacOS")
+    name = "macOS"
+
+    buildType(Release)
+})
+
+class CarbonBuildMacOS(buildName: String, configType: String, preset: String) : BuildType({
+    id(buildName.toId())
+    name = buildName
+
+    params {
+        param("env.SENTRY_CLI_DEBUG_SYMBOL_TYPE", "dsym")
+        param("env.GIT_TAG_HASH_OVERRIDE", "")
+        param("project", "eve-frontier")
+        param("env.CMAKE_CONFIG_TYPE", configType)
+        param("env.CMAKE_GENERATOR", "Ninja Multi-Config")
+        param("teamcity.vcsTrigger.runBuildInNewEmptyBranch", "true")
+        param("carbon_ref", "refs/heads/feature/kotlin")
+        param("github_checkout_folder", "github")
+        param("env.CTEST_JUNIT_OUTPUT_FILE", "ctest_results.xml")
+        select("env.VISUAL_STUDIO_PLATFORM_TOOLSET", "v141", label = "Visual Studio Platform Toolset", description = "Specify the toolset for the build. e.g. v141 or v143.",
+                options = listOf("v141 (2017)" to "v141", "v143 (2022)" to "v143"))
+        param("env.CMAKE_BUILD_TARGETS", "all")
+        param("env.CMAKE_INSTALL_PREFIX", ".build-artifact")
+        param("env.SENTRY_PROJECT", "exefile-crashes")
+        param("env.CMAKE_BUILD_FOLDER", ".cmake-build-%build.number%")
+        param("env.GIT_TAG_HASH", "")
+        param("env.EXECUTABLE_FILENAMES_MATCH", "")
+        param("env.CMAKE_PRESET", preset)
+        param("env.VCPKG_BINARY_SOURCES", "clear;x-aws,s3://vcpkg-binary-cache-static/cache/,readwrite")
+        param("env.X_VCPKG_REGISTRIES_CACHE", "%teamcity.build.checkoutDir%/%github_checkout_folder%/regcache")
+    }
+
+
+    vcs {
+        root(AbsoluteId("Carbon_PlatformPdmProtoWrapper_PlatformPdmProtoWrapper"),"+:. => %github_checkout_folder%")
+        root(AbsoluteId("CarbonPipelineTools"), "+:. => carbon_pipeline_tools")
+        cleanCheckout = true
+    }
+
+    steps {
+        exec {
+            name = "Create VCPKG registrycache location"
+            workingDir = "%teamcity.build.checkoutDir%/%github_checkout_folder%"
+            path = "mkdir"
+            arguments = "regcache"
+        }
+        exec {
+            name = "(macOS) Get Git Tag/Hash"
+            workingDir = "carbon_pipeline_tools"
+            path = "python3"
+            arguments = "carbon/GetGitTagAndOrHash.py"
+        }
+        exec {
+            name = "(macOS) Remove build artifacts folder"
+            path = "rm"
+            arguments = "-rf %env.CMAKE_INSTALL_PREFIX%"
+        }
+        exec {
+            name = "Configure"
+            path = "cmake"
+            arguments = "--preset %env.CMAKE_PRESET% -S %teamcity.build.checkoutDir%/%github_checkout_folder% -B %env.CMAKE_BUILD_FOLDER% -DCMAKE_INSTALL_PREFIX=%env.CMAKE_INSTALL_PREFIX% -DVCPKG_INSTALL_OPTIONS=--x-buildtrees-root=%teamcity.build.checkoutDir%/%github_checkout_folder%/buildtrees"
+        }
+        exec {
+            name = "Build"
+            path = "cmake"
+            arguments = "--build %env.CMAKE_BUILD_FOLDER% --config %env.CMAKE_CONFIG_TYPE% --target %env.CMAKE_BUILD_TARGETS% --parallel 8"
+        }
+        exec {
+            name = "Run Tests"
+            workingDir = "%env.CMAKE_BUILD_FOLDER%"
+            path = "ctest"
+            arguments = "-C %env.CMAKE_CONFIG_TYPE% -V --output-on-failure --output-junit %env.CTEST_JUNIT_OUTPUT_FILE% -j 8"
+        }
+        exec {
+            name = "Upload symbols to sentry"
+            path = "sentry-cli"
+            arguments = "upload-dif --wait %env.CMAKE_BUILD_FOLDER%"
+            param("script.content", """
+                #!/usr/bin/env bash -eu
+                filesWithSymbols = (
+                  # insert your binary files with symbols here!
+                )
+            """.trimIndent())
+        }
+    }
+
+    triggers {
+        vcs {
+            triggerRules = "+:root=${AbsoluteId("Carbon_PlatformPdmProtoWrapper_PlatformPdmProtoWrapper").id}:."
+
+            param("disabled", "true")
+        }
+    }
+
+    features {
+        pullRequests {
+            vcsRootExtId = "${AbsoluteId("Carbon_PlatformPdmProtoWrapper_PlatformPdmProtoWrapper")}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:06ae89f1-d5f2-4c8d-a91a-9712c233ce06"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+        commitStatusPublisher {
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:a37ec416-c03b-4ac3-a92b-ecbd3a3d8c8e"
+                }
+            }
+        }
+        xmlReport {
+            reportType = XmlReport.XmlReportType.JUNIT
+            rules = "+:%env.CMAKE_BUILD_FOLDER%/%env.CTEST_JUNIT_OUTPUT_FILE%"
+            verbose = true
+        }
+        perfmon {
+        }
+        freeDiskSpace {
+            requiredSpace = "10gb"
+            failBuild = true
+        }
+        sshAgent {
+            teamcitySshKey = "ccpgames-evetech GitHub"
+        }
+        provideAwsCredentials {
+            awsConnectionId = "Carbon_AwsVcpkgBinaryCacheServiceAccount"
+        }
+    }
+
+    requirements {
+        startsWith("teamcity.agent.jvm.os.name", "Mac OS X")
+    }
+})
+
+
