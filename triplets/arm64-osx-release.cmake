c7f2dfe6195dd58eb4aa64dcc526f5b0304157f9
ccptoebeans <ccptoebeans@users.noreply.github.com>
2026-07-21 16:25:02 +0000

    Build dependencies and wrapper with v145 on windows

---
+++ triplets/arm64-osx-release.cmake
@@ -0,0 +1,7 @@
+set(BASE_TRIPLET_FILE ${CMAKE_CURRENT_LIST_DIR}../vendor/github.com/carbonengine/vcpkg-registry/triplets/arm64-osx-release.cmake)
+
+include(BASE_TRIPLET_FILE)
+
+set(VCPKG_HASH_ADDITIONAL_FILES ${BASE_TRIPLET_FILE})
+
+set(VCPKG_LIBRARY_LINKAGE static)
---
+++ triplets/x64-windows-145-debug-static.cmake
@@ -0,0 +1,8 @@
+set(BASE_TRIPLET_FILE ${CMAKE_CURRENT_LIST_DIR}../vendor/github.com/carbonengine/vcpkg-registry/triplets/x64-windows-145-debug.cmake)
+
+include(${BASE_TRIPLET_FILE})
+
+set(VCPKG_HASH_ADDITIONAL_FILES ${BASE_TRIPLET_FILE})
+
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CRT_LINKAGE static)
