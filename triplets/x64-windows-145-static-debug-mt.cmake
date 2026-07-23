134f0e91162c490f45dce4ac86bfd91588310a30
ccptoebeans <ccptoebeans@users.noreply.github.com>
2026-07-23 11:46:18 +0000

    Create completely seperate triplets for compiling with MT static on windows

--- triplets/x64-windows-145-static-debug-mt.cmake
+++ triplets/x64-windows-145-static-debug-mt.cmake
@@ -1,8 +1,5 @@
-set(BASE_TRIPLET_FILE ${CMAKE_CURRENT_LIST_DIR}/../vendor/github.com/carbonengine/vcpkg-registry/triplets/x64-windows-145-debug.cmake)
-
-include(${BASE_TRIPLET_FILE})
-
-set(VCPKG_HASH_ADDITIONAL_FILES ${BASE_TRIPLET_FILE})
-
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_PLATFORM_TOOLSET_VERSION "14.51")
+set(VCPKG_BUILD_TYPE "release")
